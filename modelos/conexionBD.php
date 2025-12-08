bf6846f957245ece162e9f8fa9cc9b6b80f4e763
giomerida <giomerida@icloud.com>
2025-12-08 15:37:12 -0600

    Se agrego conexión de BD

---
+++ modelos/conexionBD.php
@@ -0,0 +1,15 @@
+<?php
+
+class conexionBD{
+
+    public function cBD(){
+
+        $bd = new PDO("mysql:host=localhost;bdname=universidad", "root", "");
+
+        $bd -> exec("set names uft8");
+
+        return $bd;
+
+    }
+    
+}
